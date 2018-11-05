fb27cf42578de03f1eb4a9deb2f7ab4427b2323c
weichanhyao <wyao1@stuy.edu>
2018-11-04 23:12:25 -0500

    added ordered array list, but Im fixing it

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -3,4 +3,34 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList() {
     super();
   }
-  public Ord
+  public OrderedArrayList(int startingCap){
+    super(startingCap);
+  }
+  
+  public bloolean add(T thing){
+    if (size == 0){
+      data[0] = thing;
+      return true;
+    }
+    for (int i = 0; i<size; i++){
+      if (data[i].comparesTo(thing) >= 0){
+        super.add(i, thing);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void add(int index, T thing){
+    if (data[i].comparesTo(thing) >= 0){
+        super.add(i, thing);
+    }
+  }
+  
+  public T set(int i, T thing){
+    T ans = data[i];
+    super.remove(i);
+    add(thing);
+    return T;
+  }
+}
